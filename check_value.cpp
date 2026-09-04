5332a7369379e1779e79218842f33119b44e6cdf
Kiryachenko.G.Y <Kiryachenko.G.Y@cniiag.local>
2026-09-04 17:54:22 +0300

    Исправлена ошибка сравнения на >< введённого числа и загаданного. Исправлена ошибка зависания программы при вводе нечисловой строки в качестве угадываемого числа.

--- check_value.cpp
+++ check_value.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
+#include <string>
 
 // Функция принимает число и предлагает его угадать.
 // Возвращает количество попыток, затраченных на угадывание.
 int check_value(const int target_value) {
 
 	int current_value = 0;
+    std::string input;
 	int attempts = 0;
 
 	std::cout << "Enter your guess:" << std::endl;
 
 	do {
 		attempts++;
-		std::cin >> current_value;
+        std::cin >> input;
+        current_value = std::stoi(input);
 
-        if (current_value < target_value) {
+        if (current_value > target_value) {
             std::cout << "less than " << current_value << std::endl;
 		}
-        else if (current_value > target_value) {
+        else if (current_value < target_value) {
             std::cout << "greater than " << current_value << std::endl;
 		}
 		else {
