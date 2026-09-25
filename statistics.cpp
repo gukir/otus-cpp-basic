7373d188fdd6c0a51121481196631b48e207b432
Kiryachenko.G.Y <Kiryachenko.G.Y@cniiag.local>
2026-09-25 13:44:14 +0300

    Исправлена ошибка конструктора минимального числа. Добавлен класс максимального числа.

--- statistics.cpp
+++ statistics.cpp
@@ -12,7 +12,7 @@ public:
 
 class Min : public IStatistics {
 public:
-	Min() : m_min{std::numeric_limits<double>::min()} {
+    Min() : m_min{std::numeric_limits<double>::max()} {
 	}
 
 	void update(double next) override {
@@ -31,6 +31,29 @@ public:
 
 private:
 	double m_min;
+};
+
+class Max : public IStatistics {
+public:
+    Max() : m_max{std::numeric_limits<double>::lowest()} {
+    }
+
+    void update(double next) override {
+        if (next > m_max) {
+            m_max = next;
+        }
+    }
+
+    double eval() const override {
+        return m_max;
+    }
+
+    const char * name() const override {
+        return "max";
+    }
+
+private:
+    double m_max;
 };
 
 int main() {
